6daa8dda377deb74eb532cfabf0d525d732282d5
tfwio <tfwroble@gmail.com>
2019-06-18 11:36:30 -0500

    clean up our model settings.

--- fsindex/Model.go
+++ fsindex/Model.go
@@ -5,17 +5,18 @@ var (
 	DefaultSettings = Settings{
 		OmitRootNameFromPath:       false,
 		StripFileExtensionFromName: true,
-		StripFileExtensionFromPath: false,
 		UnknownCharsToDash:         false,
 	}
 )
 
 // Settings will slightly alter how the `Refresh` method runs.
 type Settings struct {
+	// OmitRootNameFromPath will strip the root directory-name from indexed path targets.
+	// For example if true, a path converted to "http path": path-in: "c:/mypath/mysubdir/my-target-path", path-out: "/".
+	// If set to (default) false: path-in: "c:/mypath/mysubdir/my-target-path", path-out: "/my-target-path".
 	OmitRootNameFromPath       bool
 	StripFileExtensionFromName bool
-	StripFileExtensionFromPath bool
-	UnknownCharsToDash         bool
+	UnknownCharsToDash         bool // not uet supported.
 	// this tells us weather or not to use full link-path
 	// such as `http://[server:port]/` when generating JSON.
 	HardLinks bool
