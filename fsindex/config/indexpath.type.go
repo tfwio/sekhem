95eda526f2c21241a3498226f6e199ad841be7d4
tfwio <tfwroble@gmail.com>
2019-07-13 13:42:16 -0500

    add IndexPath configuration setting so that path-visibility requires login.
    there is more that we can do to secure files

--- fsindex/config/indexpath.type.go
+++ fsindex/config/indexpath.type.go
@@ -3,12 +3,13 @@ package config
 // IndexPath — same as StaticPath, however we can call on something like
 // `[target].json` to calculate/generate a file-index listing.
 type IndexPath struct {
-	Alias       string   `json:"alias,omitempty"` // FIXME: NOT USED. name of our target as opposed to our root-directory name, or default server path (in Server.Path).
-	Source      string   `json:"src"`
-	Target      string   `json:"tgt"`
-	Browsable   bool     `json:"nav"` // show directory file-listing in browser
-	Servable    bool     `json:"serve"`
-	IgnorePaths []string `json:"ignorePaths"` // absolute paths to ignore
-	Extensions  []string `json:"spec"`        // file extensions to recognize; I.E.: the `Configuration.Extensions` .Name.
-	path        string   // path as used in memory; we'll probably just ignore this guy.
+	Alias         string   `json:"alias,omitempty"` // FIXME: NOT USED. name of our target as opposed to our root-directory name, or default server path (in Server.Path).
+	Source        string   `json:"src"`
+	Target        string   `json:"tgt"`
+	Browsable     bool     `json:"nav"` // show directory file-listing in browser
+	Servable      bool     `json:"serve"`
+	RequiresLogin bool     `json:"require-logon"`
+	IgnorePaths   []string `json:"ignorePaths"` // absolute paths to ignore
+	Extensions    []string `json:"spec"`        // file extensions to recognize; I.E.: the `Configuration.Extensions` .Name.
+	path          string   // path as used in memory; we'll probably just ignore this guy.
 }
