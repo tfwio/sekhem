35ee2b83f98fa2a61d4e9ec9f3def3c692b5ebb6
tfwio <tfwroble@gmail.com>
2019-07-21 20:51:46 -0500

    documentation/clarification

--- fsindex/session/init.go
+++ fsindex/session/init.go
@@ -1,4 +1,3 @@
-
 package session
 
 import (
@@ -8,6 +7,10 @@ import (
 )
 
 // close on error (no message)
+//
+// As a precaution, do not attempt to perform complex or multiple
+// database operations — or rather perhaps, never load and attempt
+// to work with more than one loaded (.Open) database connection
 func dbinit() (*gorm.DB, bool) {
 	db, err := gorm.Open(datasys, datasource)
 	result := false
@@ -19,6 +22,10 @@ func dbinit() (*gorm.DB, bool) {
 }
 
 // no close on error (no message)
+//
+// As a precaution, do not attempt to perform complex or multiple
+// database operations — or rather perhaps, never load and attempt
+// to work with more than one loaded (.Open) database connection
 func dbinik() (*gorm.DB, bool) {
 	db, err := gorm.Open(datasys, datasource)
 	result := false
@@ -29,16 +36,28 @@ func dbinik() (*gorm.DB, bool) {
 }
 
 // closes the database and prints requested status on error.
+//
+// As a precaution, do not attempt to perform complex or multiple
+// database operations — or rather perhaps, never load and attempt
+// to work with more than one loaded (.Open) database connection
 func iniC(format string, msg ...interface{}) (*gorm.DB, bool) {
 	return inik(true, format, msg...)
 }
 
 // keep error
+//
+// As a precaution, do not attempt to perform complex or multiple
+// database operations — or rather perhaps, never load and attempt
+// to work with more than one loaded (.Open) database connection
 func iniK(format string, msg ...interface{}) (*gorm.DB, bool) {
 	return inik(false, format, msg...)
 }
 
 // closes the database and prints requested status on error.
+//
+// As a precaution, do not attempt to perform complex or multiple
+// database operations — or rather perhaps, never load and attempt
+// to work with more than one loaded (.Open) database connection
 func inik(closeOnError bool, format string, msg ...interface{}) (*gorm.DB, bool) {
 	db, e := dbinik()
 	if e {
